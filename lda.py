29e182cff93c60ac9398bd3d9bccb50e6f3d813a
SOMIL JAIN <42209500+somiljain7@users.noreply.github.com>
2023-03-01 14:28:28 +0530

    Update lda.py

--- lda.py
+++ lda.py
@@ -4,10 +4,8 @@ from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 embeddings = # input
 embeddings_normalized = (embeddings - np.mean(embeddings, axis=0)) / np.std(embeddings, axis=0)
-pca = PCA(n_components=50)
-embeddings_reduced = pca.fit_transform(embeddings_normalized)
-lda = LinearDiscriminantAnalysis(n_components=50)
-lda.fit(embeddings_reduced, labels)
+lda = LinearDiscriminantAnalysis(n_components=)
+lda.fit(embeddings_normalized, labels)
 lda.train()
 score = lda.score(embeddings_reduced, labels)
 lda.deploy()

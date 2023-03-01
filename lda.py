9647c5280e309bf4a82e33904bff13ad2d750637
SOMIL JAIN <42209500+somiljain7@users.noreply.github.com>
2023-03-01 14:26:15 +0530

    Update lda.py

--- lda.py
+++ lda.py
@@ -2,26 +2,12 @@
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-
-# Collect the embeddings from a set of known speakers
-embeddings = np.array([...])
-
-# Normalize the embeddings
+embeddings = # input
 embeddings_normalized = (embeddings - np.mean(embeddings, axis=0)) / np.std(embeddings, axis=0)
-
-# Apply a dimensionality reduction technique (such as PCA) to reduce the dimensionality of the embeddings
 pca = PCA(n_components=50)
 embeddings_reduced = pca.fit_transform(embeddings_normalized)
-
-# Train the PLDA model with the reduced embedding set
 lda = LinearDiscriminantAnalysis(n_components=50)
 lda.fit(embeddings_reduced, labels)
-
-# Estimate the model parameters (such as the maximum likelihood estimates for the means and covariance matrices of the two classes) and then optimize the model objective function
 lda.train()
-
-# Evaluate the model performance with a held-out set of embeddings
 score = lda.score(embeddings_reduced, labels)
-
-# Deploy the model in production and monitor its performance over time
 lda.deploy()
